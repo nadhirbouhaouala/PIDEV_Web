de4fcf267979f91cd9c6c7aa028a7b162574f336
Jeekyz <wael.berrachid@esprit.tn>
2020-04-12 20:07:39 +0100

    maj

--- Nozelites/app/AppKernel.php
+++ Nozelites/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new NozelitesBundle\NozelitesBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new OffreBundle\OffreBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new CMEN\GoogleChartsBundle\CMENGoogleChartsBundle(),
